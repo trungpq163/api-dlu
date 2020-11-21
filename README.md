07a28fafa63e5a51d68b326870bf0acac4f578f5
quoctrung163 <miyucoder@gmail.com>
2020-11-21 18:53:09 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,142 @@
+
+## API Schedule of Dalat University
+
+`api-dlu` is a library contain function help you essy create api :D. If you get some bug, you can create issue in repo github https://github.com/quoctrung163/api-dlu. Thank you <3
+
+## Installation
+With npm (node package manager)
+```
+npm install api-dlu --save
+```
+
+With yarn 
+```
+yarn add api-dlu
+```
+
+## Import Example
+```ts
+const {
+  handleDataScheduleToJSON,
+  performSyncScheduleFunctions,
+  urlHTMLFile
+} from 'api-dlu';
+```
+
+## Quick Start
+```js
+const express = require("express");
+const bodyparser = require("body-parser");
+const {
+  handleDataScheduleToJSON,
+  performSyncScheduleFunctions,
+  urlHTMLFile
+} = require('api-dlu');
+require("dotenv").config();
+
+const port = 8000;
+const app = express();
+
+app.use(bodyparser.json());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/", async (req, res) => {
+
+  console.log(Date.now() + " Ping Received");
+
+  if (req.query.studentID) {
+    await performSyncScheduleFunctions(req.query.studentID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+
+  if (req.query.classStudentID) {
+    await performSyncScheduleFunctions(req.query.classStudentID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+
+  if (req.query.professorID) {
+    await performSyncScheduleFunctions(req.query.professorID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+
+  if (req.query.studentID || req.query.classStudentID || req.query.professorID) {
+    let data = await handleDataScheduleToJSON();
+    res.json(data);
+    // res.sendFile(__dirname + urlHTMLFile);
+  } else {
+    const result = {
+      example: 'http://localhost:8000/?studentID={yourStudentID}&yearStudy=2019-2020&termID=HK02&week=18',
+      quick: 'http://localhost:8000/?studentID=1710289',
+      detailExample: {
+        "studentID": "yourStudentID | example: 1710289, (default: empty)",
+        "yearStudy": "example: 2019-2020, (default: current year)",
+        "termID": "example: HK01 | HK02, (default: current termID)",
+        "week": "example: 18, (default: current week)"
+      }
+    }
+    res.json(result);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://${port}/`);
+});
+```
+
+## Documentation
+### performSyncScheduleFunctions
+
+this function handle api 
+
+- `performSyncScheduleFunctions(idImport, yearImport, termImport, weekImport)`
+
+- you obligate pass value into `idImport` parameter if not you will get nothing :(
+
+- yearImport, termImport, weekImport: default if you not pass value into parameter of `performSyncScheduleFunctions` then it will get current value of (year, term, week).
+
+for example:
+```ts
+const {
+  performSyncScheduleFunctions
+} from 'api-dlu';
+
+app.get('/', async (req, res) => {
+  if (req.query.studentID) {
+    await performSyncScheduleFunctions(req.query.studentID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+
+  if (req.query.classStudentID) {
+    await performSyncScheduleFunctions(req.query.classStudentID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+
+  if (req.query.professorID) {
+    await performSyncScheduleFunctions(req.query.professorID, req.query.yearStudy, req.query.termID, req.query.week);
+  }
+})
+
+```
+
+### handleDataScheduleToJSON
+this function get json data
+for example:
+```ts
+const { handleDataScheduleToJSON } = require('api-dlu');
+
+async function dosomething() {
+  let data = await handleDataScheduleToJSON();
+  res.json(data);
+}
+```
+
+### urlHTMLFile
+get url html file in api-dlu module for rendering :D
+for example:
+```ts
+const { urlHTMLFile } = require('api-dlu');
+
+app.get('/', async (req, res) => {
+  res.sendFile(__dirname + urlHTMLFile)
+})
+```
---
+++ index.js
@@ -0,0 +1,141 @@
+const fetch = require("node-fetch");
+const { Headers } = require("node-fetch");
+const tabletojson = require("tabletojson").Tabletojson;
+
+const fs = require("fs");
+const path = require("path");
+
+require("dotenv").config();
+
+const myHeaders = new Headers();
+
+const date = new Date();
+const year = date.getFullYear();
+const month = date.getMonth();
+
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+myHeaders.append("Cookie", "ASP.NET_SessionId=wsc5km3qwjhhfh4rkey2hxac");
+
+const urlencoded = new URLSearchParams();
+urlencoded.append("txtTaiKhoan", process.env.ID);
+urlencoded.append("txtMatKhau", process.env.PassWord);
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: urlencoded,
+  redirect: "follow"
+};
+
+let yearStudy = "";
+let termID = "";
+let week = "";
+const urlHTMLFile = '/node_modules/api-dlu/index.html';
+
+const yearImportRegex = new RegExp('^[0-9]{4}-[0-9]{4}$');
+const termImportRegex = new RegExp('^(HK0[1-3])|(hk0[1-3])|(hK0[1-3])|(Hk0[1-3])$');
+
+const getYearAndTermStudy = () => {
+  if (
+    (month === 0) |
+    (month === 1) |
+    (month === 2) |
+    (month === 3) |
+    (month === 4) |
+    (month === 5) |
+    (month === 6)
+  ) {
+    yearStudy = `${year - 1}-${year}`;
+    termID = `HK02`;
+  } else if (month === 7) {
+    yearStudy = `${year - 1}-${year}`;
+    termID = `HK03`;
+  } else {
+    console.log(`${year}-${year + 1}`);
+    yearStudy = `${year}-${year + 1}`;
+    termID = `HK01`;
+  }
+};
+
+function getWeek(d) {
+  d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+  week = weekNo;
+}
+
+getYearAndTermStudy();
+getWeek(new Date());
+// getWeek();
+
+async function postLogin(url, config) {
+  await fetch(url, config)
+    .then(response => response.text())
+    .catch(error => console.log(error));
+}
+
+async function getScheduleFromURLAndWriteFile(url, config) {
+  await fetch(url, config)
+    .then(response => response.text())
+    .then(result => {
+      fs.writeFileSync(path.resolve(__dirname, "index.html"), result, "utf8", function (err) {
+        if (err) throw err;
+        else console.log("Ghi file thanh cong!");
+      });
+    })
+    .catch(error => console.log(error));
+}
+
+async function handleDataScheduleToJSON() {
+  const fileIndexHTML = await fs.readFileSync(
+    path.resolve(__dirname, "index.html"),
+    { encoding: "UTF-8" }
+  );
+  let tablesAsJson = await tabletojson.convert(fileIndexHTML, {
+    useFirstRowForHeadings: true
+  });
+  let result = await tablesAsJson[0];
+  console.log(result);
+  console.log(typeof (result));
+  return result;
+}
+
+async function performSyncScheduleFunctions(idImport, yearImport, termImport, weekImport) {
+
+  let urlLogin = await `http://online.dlu.edu.vn/Login`;
+  await postLogin(urlLogin, requestOptions);
+
+  if (idImport.indexOf('.') !== -1) {
+
+    let url = await `http://online.dlu.edu.vn/Home/DrawingProfessorSchedule?YearStudy=${yearImportRegex.test(yearImport) ? yearImport : yearStudy
+      }&TermID=${termImportRegex.test(termImport) ? termImport : termID
+      }&Week=${(typeof (Number(weekImport)) === 'number' && Number(weekImport) >= 0) ? weekImport : week
+      }&ProfessorID=${idImport}`;
+    await getScheduleFromURLAndWriteFile(url, requestOptions);
+
+  } else if (typeof (Number(idImport)) === 'number' & Number(idImport) > 0) {
+    let url = await `http://online.dlu.edu.vn/Home/DrawingStudentSchedule?StudentId=${Number(
+      idImport
+    )}&YearId=${yearImportRegex.test(yearImport) ? yearImport : yearStudy
+      }&TermId=${termImportRegex.test(termImport) ? termImport : termID
+      }&WeekId=${(typeof (Number(weekImport)) === 'number' && Number(weekImport) >= 0) ? weekImport : week
+      }`
+    await getScheduleFromURLAndWriteFile(url, requestOptions);
+
+  } else {
+    let url = await `http://online.dlu.edu.vn/Home/DrawingClassStudentSchedules_Mau2?YearStudy=${yearImportRegex.test(yearImport) ? yearImport : yearStudy
+      }&TermID=${termImportRegex.test(termImport) ? termImport : termID
+      }&Week=${(typeof (Number(weekImport)) === 'number' && Number(weekImport) >= 0) ? weekImport : week
+      }&ClassStudentID=${idImport}`;
+    await getScheduleFromURLAndWriteFile(url, requestOptions);
+  }
+
+  await handleDataScheduleToJSON();
+}
+
+module.exports = {
+  performSyncScheduleFunctions,
+  handleDataScheduleToJSON,
+  urlHTMLFile
+};
